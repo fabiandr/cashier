e769027b3c2b124bca124f4c9e0481fc43962989
Fabián Dios Rodas <fabian.dios@netexlearning.com>
2022-02-27 20:48:13 +0100

    Added discount list on construction to cashier

--- src/main/java/org/nextail/service/cashier/CashierImpl.java
+++ src/main/java/org/nextail/service/cashier/CashierImpl.java
@@ -1,8 +1,17 @@
 package org.nextail.service.cashier;
 
+import org.nextail.domain.discount.Discount;
 import org.nextail.domain.product.Product;
 
+import java.util.List;
+
 public class CashierImpl implements Cashier {
+
+    private List<Discount> discountList;
+
+    public CashierImpl(List<Discount> discountList) {
+        this.discountList = discountList;
+    }
 
     @Override
     public Float addProduct(Product product, String cartId) {
